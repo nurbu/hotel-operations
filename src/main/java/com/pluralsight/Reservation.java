d60afa4e40d707fd7e921abe5fe7697d04df03d7
Nurbu <lamanurbu2@gmail.com>
2026-05-04 13:28:42 -0400

    Completed the Reservation class with the getters and setters.

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -13,5 +13,43 @@ public class Reservation {
         this.isWeekend = isWeekend;
     }
 
-    
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getRoomPrice() {
+        if (roomType.equalsIgnoreCase("king")) {
+            return isWeekend ? 139 : 139 * 1.10;
+        }
+        return isWeekend ? 124 : 124 * 1.10;
+    }
+
+    public void setRoomPrice(double roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        return roomPrice * numberOfDays;
+    }
+
 }
