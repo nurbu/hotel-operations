d2f5b97076e97802e2f88cfd718957ff6a68f266
Nurbu <lamanurbu2@gmail.com>
2026-05-04 13:35:20 -0400

    Employee Classes completed added extra getters and setters.

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,70 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getTotalPay() {
+        return hoursWorked * payRate;
+    }
+
+    public double getRegularHours() {
+        return hoursWorked >= 40 ? 40 : hoursWorked;
+    }
+
+    public double getOvertimeHours() {
+        return hoursWorked >= 40 ? hoursWorked - 40 : 0;
+    }
+    
+}
