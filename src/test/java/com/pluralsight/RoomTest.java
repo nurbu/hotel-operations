dbbfb66e43ec018824721c898242e50bf3ae6e52
Nurbu <lamanurbu2@gmail.com>
2026-05-06 09:41:10 -0400

    Finished checking the boolean for checkIn.

--- src/test/java/com/pluralsight/RoomTest.java
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -1,5 +1,34 @@
 package com.pluralsight;
 
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 public class RoomTest {
-    
+
+    // checkIn
+    @Test
+    public void checkIn_IsOccupiedandIsDirty_True() {
+        // Arrange
+
+        Room room = new Room();
+
+        //Act
+
+        room.checkIn();
+
+        //Assert
+
+        assertEquals(true, room.isDirty());
+        assertEquals(true, room.isOccupied());
+
+    }
+    // Arrange
+
+    // Act
+
+    //Assert
+
+
+    // checkout()
 }
